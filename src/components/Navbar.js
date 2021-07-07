27d0264e5ffcdbaab7bf7024fc60c916af79c0cf
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-07 17:21:35 -0500

    add react-router functionality to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'; 
+
 function Navbar() {
     return (
         <nav>
             <h3>Logo</h3>
             <ul className='nav-links'>
-                <li className='link'>BRS</li>
-                <li className='link'>Korean War</li>
-                <li className='link'>Server Info</li>
-                <li className='link'>Flight Log</li>
+                <Link to ='/BRS'>
+                    <li className='link'>BRS</li>
+                </Link>
+                <Link to="/KoreanWar">
+                     <li className='link'>Korean War</li>
+                </Link>
+                <Link to="/ServerInfo">
+                    <li className='link'>Server Info</li>
+                </Link>
+                <Link to='/FlightLog'>
+                     <li className='link'>Flight Log</li>
+                </Link>
             </ul>
         </nav>
     )
