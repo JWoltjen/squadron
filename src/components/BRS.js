a031cb37f0f107d47c98ceb4f3bd7fb575ccfe22
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-07 22:49:46 -0500

    add content to BRS history page

--- src/components/BRS.js
+++ src/components/BRS.js
@@ -2,8 +2,125 @@ import React from 'react'
 
 function BRS() {
     return (
-        <div>
-            A History of the Bloody Red Squadron
+        <div className='history-container'>
+            <div className='history-card'>
+                <h1 className='history-title'>A History of the Bloody Red Squadron</h1>
+                <h3 className='history-subTitle'>2013-2021</h3>
+                <p>The squadron was formed within the War Thunder Arcade Beta community in June 2013. At the time, War Thunder was a new free to play massive-multiplayer online game for the PC which focused on World War II aviation. The Bloody Red Squadron (known by its in-game insignia =oBRSo=) became known for its elite cadre of so-called "Realistic Battles" pilots, especially within the nascent War Thunder jet community. At the time, the jet scene was very exclusive territory, and as a consequence, it became easy for players to become acquainted with each other. New friends and adversaries were made as F-86 Sabre flew against MiG-15bis over the skies of Korea over and over again.</p>
+                <h2>2013-2014: The Beginning</h2>
+                <h2>2014-2016: The Squadron Battles Era</h2>
+                <p>
+                    This was the golden age of War Thunder and also the squadron itself. 
+                </p>
+                <h2>2017-2019: The Twilight Era</h2>
+                <p>Sometime early within this period, JollyOldChap joined the Royal Navy submarine fleet. This left a hole in leadership that was never truly filled. This was also the period when Weisser was at college and away. Pyzro became more interested in RocketLeague, Wolf stopped playing War Thunder on a daily basis.</p>
+                <h2>2020-2021: The Dispersal</h2>
+                <p></p>
+            </div>
+            <div>
+                <ul className='notable-members'>
+                    <li>
+                        JollyOldChap_18
+                    </li>
+                    <li>
+                        BloodDragoon
+                    </li>
+                    <li>
+                        Salazaar
+                    </li>
+                    <li>
+                        Wolf_ofthe_North
+                    </li>
+                    <li>
+                        Emperor_Weisser
+                    </li>
+                    <li>
+                        iHellcat
+                    </li>
+                    <li>
+                        Pyzro
+                    </li>
+                    <li>
+                        Black_Fury
+                    </li>
+                    <li>
+                        Vulcan
+                    </li>
+                    <li>
+                        ShadowThreat
+                    </li>
+                    <li>
+                        DeathDealerDan
+                    </li>
+                     <li>
+                        PJFeihly
+                    </li>
+                    <li>
+                        Ethane103
+                    </li>
+                    <li>
+                        Deeno
+                    </li>
+                    <li>
+                        Joker
+                    </li>
+                    <li>
+                        Reconciliation
+                    </li>
+                    <li>
+                        DerpCannon
+                    </li>
+                    <li>
+                        Aylius
+                    </li>
+                     <li>
+                        Caboose
+                    </li>
+                    <li>
+                        Wiaf
+                    </li>
+                    <li>
+                        Panther
+                    </li>
+                    <li>
+                        Lodorenos
+                    </li>
+                    <li>
+                        Fire123
+                    </li>
+                    <li>
+                        Shaft
+                    </li>
+                    <li>
+                        Oxide
+                    </li>
+                    <li>
+                        KillerPenguin
+                    </li>
+                     <li>
+                        TPG
+                    </li>
+                     <li>
+                        PurpleDonut
+                    </li>
+                     <li>
+                        hrcWest
+                    </li>
+                      <li>
+                        Efren
+                    </li>
+                     <li>
+                        Pvt_Hawkins
+                    </li>
+                      <li>
+                        Cole
+                    </li>
+                     <li>
+                        Th3_Beast
+                    </li>
+                   
+                </ul>
+            </div>
         </div>
     )
 }
