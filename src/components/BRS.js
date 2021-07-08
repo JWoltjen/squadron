a103a83bb92e45d1539d0a2e3eea446aaf74dfbd
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-07 23:17:15 -0500

    add style to history page

--- src/components/BRS.js
+++ src/components/BRS.js
@@ -5,14 +5,14 @@ function BRS() {
         <div className='history-container'>
             <div className='history-card'>
                 <h1 className='history-title'>A History of the Bloody Red Squadron</h1>
-                <h3 className='history-subTitle'>2013-2021</h3>
                 <p>The squadron was formed within the War Thunder Arcade Beta community in June 2013. At the time, War Thunder was a new free to play massive-multiplayer online game for the PC which focused on World War II aviation. The Bloody Red Squadron (known by its in-game insignia =oBRSo=) became known for its elite cadre of so-called "Realistic Battles" pilots, especially within the nascent War Thunder jet community. At the time, the jet scene was very exclusive territory, and as a consequence, it became easy for players to become acquainted with each other. New friends and adversaries were made as F-86 Sabre flew against MiG-15bis over the skies of Korea over and over again.</p>
-                <h2>2013-2014: The Beginning</h2>
-                <h2>2014-2016: The Squadron Battles Era</h2>
+                <h2 className='history-subtitle'>2013-2014: The Beginning</h2>
+                <p>In the beginning the members met each other in a custom battle in War Thunder Arcade in June 18, 2013. Wolf was 28 years old at the time, Pyzro was also the same age. Weisser, Hellcat, Deeno, and Reconcilliation were in high school. Jolly and PJ were at university. The squadron predominately flew arcade battles together and communicated over a teamspeak channel someone set up. Arcade battles were a style of play which allowed a player to spawn in more than one time per match, even after dying. The gameplay was also unrealistic, and well, arcadey. Towards the end of this era, Pyzro set up a squadron website hosted on Enjin, and the squadron migrated to "Realistic Battles." </p>
+                <h2 className='history-subtitle'>2014-2016: The Squadron Battles Era</h2>
                 <p>
                     This was the golden age of War Thunder and also the squadron itself. 
                 </p>
-                <h2>2017-2019: The Twilight Era</h2>
+                <h2 className='history-subtitle'>2017-2019: The Twilight Era</h2>
                 <p>Sometime early within this period, JollyOldChap joined the Royal Navy submarine fleet. This left a hole in leadership that was never truly filled. This was also the period when Weisser was at college and away. Pyzro became more interested in RocketLeague, Wolf stopped playing War Thunder on a daily basis.</p>
                 <h2>2020-2021: The Dispersal</h2>
                 <p></p>
