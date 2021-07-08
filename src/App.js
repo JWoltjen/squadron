e6dab3ae09e309a063e3e6faaa8bd9b40b659718
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-07 21:26:52 -0500

    style background

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import Navbar from './components/Navbar'
 import BRS from './components/BRS'
 import KoreanWar from './components/KoreanWar'
 import ServerInfo from './components/ServerInfo'
+
+
+
 function App() {
   return (
 <FlightContextProvider>
   <Router>
-    <div className='App'>
     <Navbar/>
     <Switch>
       <Route path='/BRS' component={BRS}/>
       <Route path='/KoreanWar' component={KoreanWar}/>
       <Route path='/ServerInfo' component={ServerInfo}/>
     </Switch> 
+  <main className='main-container'>
     <FlightLog/>
     <FlightForm/>
-    </div>
+  </main>
   </Router>
 </FlightContextProvider>
   )
