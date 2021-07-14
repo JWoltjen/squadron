7077d2b8c013f2f916abab68bb075b63535ba33e
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-14 13:31:49 -0500

    create DropdownMenu component

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,12 @@
+import React, {useState, useRef} from 'react'
+
+const DropdownMenu = () => {
+    const dropdownRef = useRef(null); 
+    const [isActive, setIsActive] = useState(false)
+    return (
+       <>
+       </>
+    )
+}
+
+export default DropdownMenu
