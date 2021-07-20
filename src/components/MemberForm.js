bff114f3aa339e3b4b70303184b6a29818e569f1
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-20 12:37:34 -0500

    add pilot stats to member form card

--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -9,6 +9,11 @@ function MemberForm() {
     const [description, setDescription] = useState('')
     const [nationality, setNationality] = useState('')
     const [rank, setRank] = useState('')
+    const [eyeSight, setEyeSight] = useState(0)
+    const [awareness, setAwareness] = useState(0)
+    const [aggression, setAggression] = useState(0)
+    const [communication, setCommunication] = useState(0)
+    const [gunnery, setGunnery] = useState(0)
 
     const handleChange = e => {
         setDescription(e.target.value)
@@ -16,14 +21,19 @@ function MemberForm() {
     const handleSubmit = e => {
         e.preventDefault(); 
         if(editItem === null){
-            addMember(name, image, nationality, rank, description)
+            addMember(name, image, nationality, rank, description, eyeSight, awareness, aggression, communication, gunnery)
             setName('')
             setImage('')
             setNationality('')
             setRank('')
             setDescription('')
+            setEyeSight(0)
+            setAwareness(0)
+            setAggression(0)
+            setCommunication(0)
+            setGunnery(0)
         } else {
-            editMember(name, image, nationality, rank, description, editItem.id)
+            editMember(name, image, nationality, rank, description, eyeSight, awareness, aggression, communication, gunnery, editItem.id)
         } 
     }
     const updateNationality = e => {
@@ -38,6 +48,21 @@ function MemberForm() {
       const updateImage = e => {
         setImage(e.target.value)
     }
+    const updateEyeSight = e => {
+        setEyeSight(e.target.value)
+    }
+    const updateAwareness = e => {
+        setAwareness(e.target.value)
+    }
+    const updateAggression = e => {
+        setAggression(e.target.value)
+    }
+    const updateCommunication = e => {
+        setCommunication(e.target.value)
+    }
+    const updateGunnery = e => {
+        setGunnery(e.target.value)
+    }
 
 
     useEffect(() => {
@@ -47,7 +72,11 @@ function MemberForm() {
             setNationality(editItem.nationality)
             setRank(editItem.rank)
             setDescription(editItem.description)
-            console.log(editItem)
+            setEyeSight(editItem.eyeSight)
+            setAwareness(editItem.awareness)
+            setAggression(editItem.aggression)
+            setCommunication(editItem.communication)
+            setGunnery(editItem.gunnery)
         } else {
             setDescription('')
         }
@@ -91,6 +120,46 @@ function MemberForm() {
                             type='text' 
                             onChange={updateRank}
                             placeholder='Rank'
+                            required
+                            />
+                            <input 
+                            type='range'
+                            onChange={updateEyeSight}
+                            placeholder='Eyesight'
+                            min="0"
+                            max='5'
+                            required
+                            />
+                            <input 
+                            type='range'
+                            onChange={updateAwareness}
+                            placeholder='Awareness'
+                            min="0"
+                            max='5'
+                            required
+                            />
+                            <input 
+                            type='range'
+                            onChange={updateAggression}
+                            placeholder="Aggression"
+                            min="0"
+                            max='5'
+                            required
+                            />
+                            <input
+                            type='range'
+                            onChange={updateCommunication}
+                            placeholder='Communication'
+                            min="0"
+                            max='5'
+                            required
+                            />
+                            <input 
+                            type='range'
+                            onChange={updateGunnery}
+                            placeholder='Gunnery'
+                            min="0"
+                            max='5'
                             required
                             />
                             </div>
