ae1eb8f16c12697ed45b1f5bf25e1052f4c7d33b
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-20 19:14:09 -0500

    add images to photogallery page

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import BigIron from '../images/BigIron.png'
+import bigiron1 from '../images/bigiron1.png'
+import bigiron2 from '../images/bigiron2.png'
+import bigiron3 from '../images/bigiron3.png'
+import bigiron4 from '../images/bigiron4.png'
 
 function PhotoGallery() {
     return (
         <div>
-            PhotoGallery goes here
+            <img src={BigIron}/>
+            <img src={bigiron1}/>
+            <img src={bigiron2}/>
+            <img src={bigiron3}/>
+            <img src={bigiron4}/>
         </div>
     )
 }
--- src/contexts/SquadronContext.js
+++ src/contexts/SquadronContext.js
@@ -6,20 +6,6 @@ export const SquadronListContext = createContext()
 const SquadronContextProvider = props => {
         const initialState = JSON.parse(localStorage.getItem('members'))
 
-        // const initialState = 
-        // [{
-        //     id: uuidv4(),
-        //     name: 'Wolf', 
-        //     image: 'https://www.langleyadvancetimes.com/wp-content/uploads/2017/04/5907langleymiller-vintage.jpg',
-        //     nationality: "United States", 
-        //     rank: 'Lt. Colonel', 
-        //     description: 'Founding member of the Bloody Red Squadron in June 2013. Pioneer of online F-86 combat, first in War Thunder and later on in DCS. He displayed an obsessive relationship with the F-86F, scoring over 5000 victories with it in War Thunder over a 3-year period. After some time away, in early 2021, Wolf returned to DCS to pilot the Sabre on Korea 1952. There, he quickly developed a reputation as the server\'s best F-86 pilot, scoring an average of 20 victories a month.', 
-        //     eyesight: 5,
-        //     awareness: 5, 
-        //     aggression: 5, 
-        //     communication: 3, 
-        //     gunnery: 2
-        // }]
     const [members, setMembers] = useState(initialState)
 
     useEffect(() => {
