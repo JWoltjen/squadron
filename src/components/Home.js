a0b7d3310af6d8bf77a488b1b6429da1c4326136
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-27 15:17:04 -0500

    add text and image to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import Footer from './Footer'
+import BigIron from '../images/BigIron.png'
+
 function Home() {
     return (
         <div className='home-container'>
+            <h1>Welcome to Squadron!</h1>
+            <img className='hero-welcome' src={BigIron}/>
+            <p>Squadron is a single-page application you can use to keep track of your flights and teammates from your favorite virtual pilot games.
+              It allows players to assign teammates, and even give them customizable ratings! 
+            </p>
         </div>
     )
 }
